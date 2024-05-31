fa2521a67606b5e1bf810bc47def0fef4899da50
sebDtSci <sebastientadiello@gmail.com>
2024-05-31 15:45:47 +0200

    refactoring

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='SpeachToSpeach',
+    version='0.1.0',
+    description="",
+    author="Tadiello Sébastien",
+    author_email="sebastientadiello@gmail.com",
+    packages=find_packages(),
+    install_requires=requirements,
+    # test_suite='test',
+)
